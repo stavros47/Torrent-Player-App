ec6bd46f6c38729bcbf97d71fe578ef7daa976f7
SArgyrou <stavris.47@gmail.com>
2018-07-23 02:58:26 +0300

    Scrape Movie Posters from IMDB (too small - should fix)

--- scraper.js
+++ scraper.js
@@ -1,9 +1,12 @@
 const cheerio = require('cheerio');
 const Table = require('cli-table');
 const fetch = require('node-fetch');
+var ptn = require('parse-torrent-name');
+
 
 var options = {
-    url:"https://thepiratebay.org/top/201",
+    imdbURL:'https://www.imdb.com/find?s=all&q=',
+    url:"https://thepiratebay.org/top/207",
     json: false
 }
 
@@ -13,13 +16,41 @@ var table = new Table({
     style: { 'padding-left': 0, 'padding-right': 0 }
 });
 
+const movies = [];
 
 //get piratebay movies and their links
 function getMovies(){
     return fetch(`${options.url}`)
     .then(response => response.text())
     .then(body => {
-        const movies = [];
+       
+        const $ = cheerio.load(body);
+       
+        var rows = $("#searchResult").children('tbody').children();
+        rows.each(function(i, element){
+            const $titleRow = $(this).children('td').eq(1).find(".detName").find("a");
+            const $title = $titleRow.text();
+            const $link = $(this).children('td').eq(1).children('a').eq(0).attr('href');
+            if($title){
+                
+                const movie = {
+                    title: ptn($title).title,
+                    link: $link
+                }
+                movies.push(movie);
+            }
+            
+        });
+        
+        return movies;
+    });
+}
+
+function getMoviePoster(){
+    return fetch(`${options.url}`)
+    .then(response => response.text())
+    .then(body => {
+       
         const $ = cheerio.load(body);
        
         var rows = $("#searchResult").children('tbody').children();
@@ -36,7 +67,7 @@ function getMovies(){
             }
             
         });
-      
+       
         return movies;
     });
 }
@@ -63,13 +94,33 @@ function getTopMovies(){
     });
 }
 
+
 function searchMovie(searchTerm){
-    
+    return fetch(`${options.imdbURL}${searchTerm.title}`)
+    .then(response => response.text())
+    .then(body =>{
+        const $ = cheerio.load(body);
+        // $('.findResult').each(function(i,element){
+        //     const $element = $(element);
+        //     const $image = $element.find('td a img');
+        //     let image = $image.attr('src');
+        // });
+        
+            const $element = $('.findResult');
+            const $image = $element.find('td a img');
+            let image = $image.attr('src');
+            searchTerm.image = image;
+            
+            return searchTerm;
+    });
 }
+
+
 
 module.exports = {
     getTopMovies,
-    getMovies
+    getMovies,
+    searchMovie
 }
 
 
